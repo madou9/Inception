f0e4bce47cfdb5606d4d6c47d5b1e30fb1526d29
madou9 <issoufou.mamadou@yahoo.com>
2024-06-03 23:18:06 +0200

    replace for exemple the password to the name that it has inside the dotenv

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -2,9 +2,13 @@
 cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
+echo $DB_HOST
+echo $DB_USER
+echo $DB_PASS
+echo $DB_NAME
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress_database --dbuser=wordpress_Hama --dbpass=pass --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=ihama.42.fr --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --allow-root
+./wp-cli.phar core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=$WP_EMAIL --allow-root
 
 php-fpm8.2 -F
 
