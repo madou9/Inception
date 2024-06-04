0c430e0b672f7d44d70453cc55e19bba59f12331
madou9 <issoufou.mamadou@yahoo.com>
2024-06-04 13:14:51 +0200

    trying to solved environment for mariadb

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Use environment variable substitution with sed (less recommended approach)
+sed -i "s/WORDPRESS_DATABASE/$DB_NAME/g" /etc/mysql/init.sql && \
+  sed -i "s/wordpress_Hama/$DB_USER/g" /etc/mysql/init.sql && \
+  sed -i "s/pass/$DB_PASS/g" /etc/mysql/init.sql
+
 mysql_install_db
 mysqld
+
