1637b9c3f5cc4515b486b87cd825893937a1e2e9
madou9 <issoufou.mamadou@yahoo.com>
2024-06-11 15:49:38 +0200

    Add script for creating a second user

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -9,6 +9,8 @@ echo $DB_NAME
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --allow-root
 ./wp-cli.phar core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=$WP_EMAIL --allow-root
+# Create an additional user (replace 'new_username' and 'new_user_email' with desired values)
+./wp-cli.phar user create ${W_USER1} ${W_UEMAIL} --user_pass=${W_UPASS} --role=author
 
 php-fpm8.2 -F
 
